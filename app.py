fab98620a7c137576fb185c390ef051746628d9d
pawelofficial <zdunekpawel@protonmail.com>
2023-04-09 12:44:37 +0200

    app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+# foo
+@app.route("/")
+def hello():
+    return "<html><body><h1>Hello Best Bike App! update</h1></body></html>\n"
+
+@app.route("/endpoint")
+def endpoint():
+    return "<html><body><h1>Hello there my friend !</h1></body></html>\n"
+
+if __name__ == '__main__':
+    app.run(port=80)
